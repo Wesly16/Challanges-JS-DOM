8f6f08b4daaf5511c511bbc8d80b3fa616f31be9
Wesly Daud <weslydaud02@gmail.com>
2023-04-06 18:51:44 +0700

    section4/section4.js

--- section4/section4.js
+++ section4/section4.js
@@ -1,16 +1,16 @@
 // membuat elemen baru
 var newListItem = document.createElement("li");
-var textNode = document.createTextNode("Kata - kata 4");
+var textNode = document.createTextNode("Tulisan ke-4");
 newListItem.appendChild(textNode);
 document.getElementById("list").appendChild(newListItem);
 
 // membuat elemen image baru
 var newImage = document.createElement("img");
-newImage.setAttribute("src", "https://res.cloudinary.com/teepublic/image/private/s--TLzsjSTQ--/t_Preview/b_rgb:36538b,c_limit,f_jpg,h_630,q_90,w_630/v1523266999/production/designs/2572232_0.jpg");
+newImage.setAttribute("src", "https://tse4.mm.bing.net/th?id=OIP.BMDq38G7vr2bYc-yft241gHaD7&pid=Api&P=0");
 document.getElementById("div").appendChild(newImage);
 
 // membuat elemen button baru
 var newButton = document.createElement("button");
-var textNode = document.createTextNode("Klik Aku");
+var textNode = document.createTextNode("Klik Disini");
 newButton.appendChild(textNode);
 document.body.appendChild(newButton);
