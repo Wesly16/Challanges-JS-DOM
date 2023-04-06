5859fb53eaa7bffb25f9b11112c1da9a62dca65d
Wesly Daud <weslydaud02@gmail.com>
2023-04-06 19:00:10 +0700

    section8/section8.js

--- section8/section8.js
+++ section8/section8.js
@@ -1,13 +1,13 @@
 const maleRadio = document.getElementById("maleradio");
 const femaleRadio = document.getElementById("femaleradio");
 
-maleRadio.addEventListener("jenis kelamin", function() {
+maleRadio.addEventListener("jenis kelamin", function () {
   if (maleRadio.checked) {
     console.log("Anda memilih laki-laki");
   }
 });
 
-femaleRadio.addEventListener("jenis kelamin", function() {
+femaleRadio.addEventListener("jenis kelamin", function () {
   if (femaleRadio.checked) {
     console.log("Anda memilih perempuan");
   }
@@ -15,38 +15,38 @@ femaleRadio.addEventListener("jenis kelamin", function() {
 
 // Contoh 2
 
-var radios = document.getElementsByName('color');
+var radios = document.getElementsByName("color");
 
 for (var i = 0; i < radios.length; i++) {
-	radios[i].addEventListener('change', function() {
-		var selectedValue = document.querySelector('input[name="color"]:checked').value;
-		console.log('Warna yang anda pilih:', selectedValue);
-	});
+  radios[i].addEventListener("change", function () {
+    var selectedValue = document.querySelector('input[name="color"]:checked').value;
+    console.log("Warna yang anda pilih adalah:", selectedValue);
+  });
 }
 
 // Contoh 3
-const inputField = document.getElementById('masukkan');
-const output = document.getElementById('output');
+const inputField = document.getElementById("masukkan");
+const output = document.getElementById("output");
 
-inputField.addEventListener('input', (event) => {
+inputField.addEventListener("input", (event) => {
   const inputValue = event.target.value;
   output.innerText = `Kamu mengetik: ${inputValue}`;
-  output.style.display = 'block';
+  output.style.display = "block";
 });
 
 // Contoh 4
-const myInput = document.getElementById('myinput');
-const myOutput = document.getElementById('myoutput');
+const myInput = document.getElementById("myinput");
+const myOutput = document.getElementById("myoutput");
 
-myInput.addEventListener('input', (event) => {
+myInput.addEventListener("input", (event) => {
   const inputValue = event.target.value;
   if (inputValue < 0) {
-    myOutput.innerText = 'Masukan nilai minimal harus 0';
-    myOutput.style.display = 'block';
+    myOutput.innerText = "Masukan nilai minimal harus 0";
+    myOutput.style.display = "block";
   } else if (inputValue > 100) {
-    myOutput.innerText = 'Masukan nilai maksimal 100';
-    myOutput.style.display = 'block';
+    myOutput.innerText = "Masukan nilai maksimal 100";
+    myOutput.style.display = "block";
   } else {
-    myOutput.style.display = 'none';
+    myOutput.style.display = "none";
   }
 });
